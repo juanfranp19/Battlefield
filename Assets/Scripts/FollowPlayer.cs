b0a2daedb56b695c7899748b81479e29c0f20ba9
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-11-23 03:52:58 +0100

    camara sigue a player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    public Vector3 desplazamiento = new Vector3(0, 10, 11);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // asigna la posición del player
+        this.transform.position = player.transform.position + desplazamiento;
+    }
+}
